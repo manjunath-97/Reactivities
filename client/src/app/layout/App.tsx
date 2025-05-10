37bc6688f483f386d0cdd538e7e753b59a3a9679
Manjunatha C <manjunatha.c@aurigo.com>
2025-05-10 16:16:50 +0530

    Checking in React Query, Axios changes

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,27 +1,21 @@
 import { Box, Container, CssBaseline, Typography } from "@mui/material";
-import axios from "axios";
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import NavBar from "./Navbar";
 import ActivityDashboard from "../../features/activities/ActivityDashboard";
+import { useActivities } from "../../lib/hooks/useActivities";
 
 function App() {
 
-    const [activities, setActivities] = useState<Activity[]>([]);
 
     const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
     const [editMode, setEditMode] = useState<boolean>(false);
 
-
-    useEffect(() => {
-        axios.get<Activity[]>('https://localhost:5001/api/Activities').
-            then(response => setActivities(response.data))
-
-        return () => {}
-    }, [])
+    // Queries
+    const { activities, isPending } = useActivities();
 
     const handleSelectActivity = (id : string) => {
-        const activity = activities.find(x => x.id === id);
+        const activity = activities!.find(x => x.id === id);
         setSelectedActivity(activity);
     };
 
@@ -44,43 +38,45 @@ function App() {
     }
 
     const submitForm = (activity: Activity) => {
-        if (activity.id)
-        {
-            setActivities(activities.map(a => a.id === activity.id ? activity : a));
-            setSelectedActivity(activity);
-        }
-        else {
-            const newActivity = { ...activity, id: activities.length.toString() }
-            setActivities([...activities, newActivity]);
-            setSelectedActivity(newActivity);
-        }
+        //if (activity.id) {
+        //    setActivities(activities.map(a => a.id === activity.id ? activity : a));
+        //    setSelectedActivity(activity);
+        //}
+        //else {
+        //    const newActivity = { ...activity, id: activities.length.toString() }
+        //    setActivities([...activities, newActivity]);
+        //    setSelectedActivity(newActivity);
+        //}
+        console.log(activity);
         setEditMode(false);
 
     }
 
     const deleteActivity = (id: string) => {
-        setActivities(activities.filter(a => a.id !== id));
-        setSelectedActivity(undefined);
+        console.log(id);
     }
 
 
     return (
-        <Box sx={{bgcolor:"#eeeeee"} }>
+        <Box sx={{bgcolor:"#eeeeee", minHeight:'10vh'} }>
             <CssBaseline/>
             <NavBar openForm={HandleOpenForm} />
-            <Container maxWidth='xl' sx={{mt: 3}}>
-                <Typography variant='h3'>Reactivities</Typography>
-                <ActivityDashboard
-                    activities={activities}
-                    selectedActivity={selectedActivity}
-                    selectActivity={handleSelectActivity}
-                    cancelSelectActivity={handleCancelSelectActivity}
-                    editMode={editMode}
-                    openForm={HandleOpenForm}
-                    closeForm={HandleCloseForm}
-                    submitForm={submitForm}
-                    deleteActivity={deleteActivity}
-                />
+            <Container maxWidth='xl' sx={{ mt: 3 }}>
+
+            {(!activities || isPending) ?
+                    (<Typography>Loading...</Typography>) :
+                    (<ActivityDashboard
+                        activities={activities}
+                        selectedActivity={selectedActivity}
+                        selectActivity={handleSelectActivity}
+                        cancelSelectActivity={handleCancelSelectActivity}
+                        editMode={editMode}
+                        openForm={HandleOpenForm}
+                        closeForm={HandleCloseForm}
+                        submitForm={submitForm}
+                        deleteActivity={deleteActivity}
+                    />)
+                }
             </Container>
         </Box>
   )
---
+++ client/src/lib/hooks/useActivities.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import agent from "../api/agent";
+
+export const useActivities = () => {
+    const { data: activities, isPending } = useQuery({
+        queryKey: ['activities'],
+        queryFn: async () => {
+            const response = await agent.get<Activity[]>('/Activities');
+            return response.data;
+        }
+    })
+
+    return {
+        activities,
+        isPending
+    }
+}
